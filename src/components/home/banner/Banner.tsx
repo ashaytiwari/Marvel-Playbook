11819f721ec1b8ed2b11c2cbf30aca5e87f27ce2
ashaytiwari <ashaytiwari2001@gmail.com>
2022-06-14 13:41:37 +0530

    feat: changed layout for home page banner

--- src/components/home/banner/Banner.tsx
+++ src/components/home/banner/Banner.tsx
@@ -11,18 +11,38 @@ const Banner = () => {
     loop: 0
   });
 
-  return (
-    <div className={styles.homeBannerMain}>
+  function renderBannerContent() {
 
+    const seeMoreControlAttributes = {
+      className: styles.seeMoreControl,
+      href: 'https://www.marvel.com/articles/movies/thor-love-and-thunder-character-posters',
+      target: '_blank'
+    };
+
+    return (
       <div className={styles.bannerContent}>
+
         <h1 className={styles.bannerText}>
           Turn your ideas into {' '}
           <span className={styles.typewriterContent}>{text}</span>
           <span className={styles.typewriterCursor}><Cursor cursorStyle='|' /></span>
         </h1>
-      </div>
 
-      <div className={styles.bannerImageWrapper}></div>
+        <p className={styles.bannerDescription}>Check out new character posters, and get your tickets now for Thor: Love and Thunder!</p>
+
+        <a {...seeMoreControlAttributes}>See more</a>
+
+      </div>
+    );
+
+  }
+
+  return (
+    <div className={styles.homeBannerMain}>
+
+      <div className={styles.bannerImageWrapper}>
+        {renderBannerContent()}
+      </div>
 
     </div>
   );
