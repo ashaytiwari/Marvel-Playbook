86d97799bf7d236e4f9bc0b660093c40993639e7
ashaytiwari <ashaytiwari2001@gmail.com>
2022-11-11 12:27:33 +0530

    UI: created characters page banner

--- src/pages/characters/Characters.tsx
+++ src/pages/characters/Characters.tsx
@@ -4,8 +4,21 @@ import styles from './Characters.module.scss';
 
 function Characters() {
 
+  function renderCharactersBanner() {
+
+    return (
+      <div className={styles.charactersBanner}>
+        <h1 className={styles.charactersHeading}>Marvel Characters</h1>
+        <label className={styles.charactersLabel}>Get hooked on a hearty helping of heroes and villains from the humble House of Ideas!</label>
+      </div>
+    );
+
+  }
+
   return (
-    <div>Characters</div>
+    <div id={styles.charactersMain}>
+      {renderCharactersBanner()}
+    </div>
   );
 
 }
